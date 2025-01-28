0f60bbaabc37726ef0b775316cc2d78f5050fc74
Melany <mefrancom@unal.edu.co>
2025-01-27 23:31:09 -0500

    menú listo

--- src/componentes/Menu/Menu.jsx
+++ src/componentes/Menu/Menu.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import "./menu.css";
 
-function Menu () {
-    return (
-        <div>
-            <h1>Menu</h1>
+function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className='todo'>
+      <button onClick={toggleMenu} className="menu-icon">
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </button>
+      {isOpen && (
+        <div className="menu-content">
+          <ul className='listas'>
+            <li className='listaMenu'>Inicio</li>
+            <li className='listaMenu'>Catálogo de ejercicios</li>
+            <li className='listaMenu'>Contacto</li>
+            <li className='listaMenu'>Sugerencias</li>
+          </ul>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Menu;
