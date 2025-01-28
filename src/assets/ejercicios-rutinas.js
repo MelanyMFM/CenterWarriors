2ce15cd9e899915545de6f1b3cf4f0889a10f2a0
Melany <mefrancom@unal.edu.co>
2025-01-28 18:08:46 -0500

    cosas

---
+++ src/assets/ejercicios-rutinas.js
@@ -0,0 +1,22 @@
+export default [
+      {
+        "id": 1,
+        "nombre": "Rutina 1",
+        "entrenador": "Entrenador 1",
+        "ejercicios": [
+          {"id": 101, "nombre": "Ejercicio 1", "completado": false},
+          {"id": 102, "nombre": "Ejercicio 2", "completado": false},
+          {"id": 103, "nombre": "Ejercicio 3", "completado": false}
+        ]
+      },
+      {
+        "id": 2,
+        "nombre": "Rutina 12",
+        "entrenador": "Entrenador 1",
+        "ejercicios": [
+          {"id": 201, "nombre": "Ejercicio 1", "completado": false},
+          {"id": 202, "nombre": "Ejercicio 2", "completado": false},
+          {"id": 203, "nombre": "Ejercicio 3", "completado": false}
+        ]
+      }
+    ]
